389e0a2f3c435bc1d7cd6b49e564ae22b7efc2a9
Alex <aag147@live.dk>
2018-06-07 14:47:27 +0200

    rpn generator bug fix

--- detection/data/rpn_generators.py
+++ detection/data/rpn_generators.py
@@ -73,7 +73,7 @@ class DataGenerator():
             imageMeta = self.imagesMeta[imageID]
             imageMeta['id'] = imageID
             
-            path = self.cfg.data_path +'anchors/train/' + imageMeta['imageName'].split('.')[0]
+            path = self.cfg.data_path +'anchors/train/' + imageMeta['imageName'].split('.')[0] + '.JSON'
             if os.path.exists(path):
                 return None, None, None, None, None
             
