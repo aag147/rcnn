88aae11cf220a0886cb3208c9d338b0e9e80da6e
Alex <aag147@live.dk>
2018-06-22 15:14:19 +0200

    fewer prints in count_rpn_p.. (2)

--- detection/tests/count_rpn_positive_proposals.py
+++ detection/tests/count_rpn_positive_proposals.py
@@ -39,7 +39,7 @@ for i in range(genTrain.nb_batches):
     nb_pos = np.sum(deltas) / 4
     counts[i] = nb_pos
     
-    if i % 1000:
+    if i % 1000 == 0:
         utils.update_progress_new(i+1, genTrain.nb_batches, imageID + ': ' + str(nb_pos))
     
     
