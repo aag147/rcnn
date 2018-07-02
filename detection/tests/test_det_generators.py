bfb0bd32cef467d258869fd28ba4a33cbc00add1
Alex <aag147@live.dk>
2018-07-02 15:32:03 +0200

    train det input bf6

--- detection/tests/test_det_generators.py
+++ detection/tests/test_det_generators.py
@@ -4,52 +4,51 @@ Created on Mon May  7 15:40:50 2018
 
 @author: aag14
 """
-if False:
-    import sys 
-    sys.path.append('../../../')
-    sys.path.append('../../')
-    sys.path.append('../../shared/')
-    sys.path.append('../models/')
-    sys.path.append('../filters/')
-    sys.path.append('../data/')
-    
-    import numpy as np
-    
-    import utils,\
-           extract_data,\
-           methods,\
-           losses,\
-           callbacks,\
-           filters_helper as helper
-    from det_generators import DataGenerator
-    
+import sys 
+sys.path.append('../../../')
+sys.path.append('../../')
+sys.path.append('../../shared/')
+sys.path.append('../models/')
+sys.path.append('../filters/')
+sys.path.append('../data/')
 
-    # meta data
-    data = extract_data.object_data(False)
-    
-    # config
-    cfg = data.cfg
-    obj_mapping = data.class_mapping
+import numpy as np
 
-    # data
-#    genTrain = DataGenerator(imagesMeta = data.trainGTMeta, cfg=cfg, data_type='train', do_meta=False)
-genVal = DataGenerator(imagesMeta = data.valGTMeta, cfg=cfg, data_type='val', do_meta=True)
+import utils,\
+       extract_data,\
+       methods,\
+       losses,\
+       callbacks,\
+       filters_helper as helper
+from det_generators import DataGenerator
 
 
-genItr = genVal.begin()
-for batchidx in range(genVal.nb_batches):
+# meta data
+data = extract_data.object_data(False)
+
+# config
+cfg = data.cfg
+obj_mapping = data.class_mapping
+
+# data
+genTrain = DataGenerator(imagesMeta = data.trainGTMeta, cfg=cfg, data_type='train', do_meta=False)
+#genVal = DataGenerator(imagesMeta = data.valGTMeta, cfg=cfg, data_type='val', do_meta=True)
+
+
+genItr = genTrain.begin()
+for batchidx in range(genTrain.nb_batches):
     [X,rois], y, imageMeta, imageDims, _ = next(genItr)
 #    if batchidx+1 % 100 == 0:
-    utils.update_progress_new(batchidx+1, genVal.nb_batches, '')
+    utils.update_progress_new(batchidx+1, genTrain.nb_batches, '')
     
-    import draw
-    import filters_detection
-    
-    img = np.copy(X[0])
-    img += cfg.PIXEL_MEANS
-    img = img.astype(np.uint8)
-    rois = filters_detection.unprepareInputs(rois, imageDims)
-    
-    draw.drawOverlapAnchors(img, rois[0], imageMeta, imageDims, cfg)
-    
-    break
+#    import draw
+#    import filters_detection
+#    
+#    img = np.copy(X[0])
+#    img += cfg.PIXEL_MEANS
+#    img = img.astype(np.uint8)
+#    rois = filters_detection.unprepareInputs(rois, imageDims)
+#    
+#    draw.drawOverlapAnchors(img, rois[0], imageMeta, imageDims, cfg)
+#    
+#    break
