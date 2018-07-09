6d7aa745aecdeddf5abe06fc6b13ff41a2a2038e
Alex <aag147@live.dk>
2018-07-09 10:08:17 +0200

    even fewer prints in save hoi input

--- detection/models/hoi_test.py
+++ detection/models/hoi_test.py
@@ -30,7 +30,7 @@ def saveInputData(generator, Stages, cfg):
 #        [img,proposals], y, imageMeta, imageDims, times = next(genIterator)
         X, y, imageMeta, imageDims, times = next(genIterator)
         imageID = imageMeta['imageName'].split('.')[0]
-        if batchidx+1 % 500 == 0 or batchidx==50:
+        if batchidx+1 % 1000 == 0 or batchidx==50:
             utils.update_progress_new(batchidx+1, generator.nb_batches, imageID)
         
         #STAGE 1
