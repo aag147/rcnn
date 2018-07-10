9a4e1af1ce5d056ca145a9d353513b440f9fbda9
Alex <aag147@live.dk>
2018-07-10 22:48:26 +0200

    less prints

--- detection/models/hoi_test.py
+++ detection/models/hoi_test.py
@@ -31,7 +31,7 @@ def saveInputData(generator, Stages, cfg):
 #        [img,proposals], y, imageMeta, imageDims, times = next(genIterator)
         X, y, imageMeta, imageDims, times = next(genIterator)
         imageID = imageMeta['imageName'].split('.')[0]
-        if batchidx % 1 == 0 or batchidx==100 or batchidx==250:
+        if batchidx % 500 == 0 or batchidx==100 or batchidx==250:
             utils.update_progress_new(batchidx, generator.nb_batches, imageID)
         
         
